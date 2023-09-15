44fdfbd79e26a1d1f1bd0e88897ebfebf2349275
Abdullah-1922 <kafikafi1922@gmail.com>
2023-09-16 02:57:03 +0600

    Total credit section done

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -6,39 +6,54 @@ import swal from 'sweetalert';
 
 const Home = () => {
   const [Courses, setCourses] = useState([]);
-const [selectedCourses,setSelectedCourses]=useState([])
+  const [selectedCourses, setSelectedCourses] = useState([]);
+  const [totalCredit ,setTotalCredit]=useState(0)
   useEffect(() => {
     fetch('./data.json')
       .then((res) => res.json())
       .then((data) => setCourses(data));
   }, []);
   
-  const handleSelected =(selectedCourse)=>{
- const alreadyExist = selectedCourses.find(course => course.Title == selectedCourse.Title)
- if(alreadyExist){
-   return swal("Already selected", "", "error");
-}else{
-    const newSelectedCourse =[...selectedCourses,selectedCourse]
-      setSelectedCourses(newSelectedCourse)}
+  const handleSelected = (selectedCourse) => {
+let totalCredit = parseInt(selectedCourse.Credit);
 
-        
-  }
+
+    const alreadyExist = selectedCourses.find(
+      (course) => course.Title == selectedCourse.Title,
+    );
+
+    if (alreadyExist) {
+      return swal('Already selected', '', 'error');
+    } else {  
+
+        selectedCourses.forEach(item=>{
+            totalCredit+= parseInt(item.Credit)
+        })
+       if(totalCredit>20){
+        return swal('Limit is 20hr', '', 'error');
+       }
+       setTotalCredit(totalCredit);
+      const newSelectedCourse = [...selectedCourses, selectedCourse];
+      setSelectedCourses(newSelectedCourse);
+    }
+  };
 
   return (
     <div className='container mx-auto flex gap-4'>
-    <div className='grid gap-10 w-[70%] md:w-3/4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-      {Courses.map(course => (
-        <Course
-        key={course.id}
-        handleSelected={handleSelected}
-        Courses={course}
-        ></Course>
-      ))}
-    </div>
+      <div className='grid gap-10 w-[70%] md:w-3/4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+        {Courses.map((course) => (
+          <Course
+            key={course.id}
+            handleSelected={handleSelected}
+            Courses={course}></Course>
+        ))}
+      </div>
       <div className='w-[30%] md:w-1/4 pl-1 lg:pl-14'>
         <SelectedCourse
-        selectedCourses={selectedCourses}
-        ></SelectedCourse>
+        totalCredit={totalCredit}
+         selectedCourses={selectedCourses}>
+
+         </SelectedCourse>
       </div>
     </div>
   );
--- src/Components/selectedCourse/SelectedCourse.jsx
+++ src/Components/selectedCourse/SelectedCourse.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 
-const SelectedCourse = ({ selectedCourses }) => {
+const SelectedCourse = ({ selectedCourses ,totalCredit}) => {
   
   let num = 0;
   const number = () => {
@@ -10,17 +10,17 @@ const SelectedCourse = ({ selectedCourses }) => {
   return (
     <div>
         <h4 className="text-3xl font-semibold">Course Name</h4>
-        <div className="mt-5">
+        <div className="mt-8">
             {selectedCourses.map((course) => (
         <div  key={course.id}>
             
-          <p className="text-lg font-medium">
+          <p className="text-lg ">
             {number()}. {course.Title}
           </p>
         </div>
       ))}
       </div>
-      
+      <p  className="text-xl mt-10 font-medium">Total Credit Hour: {totalCredit}</p>
     </div>
   );
 };
